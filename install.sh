9379bb087c3cf07427e14ba108f21e85280376ca
Andrea <me@andreacorriga.com>
2017-07-11 17:49:08 +0200

    Corretti i riferimenti alla libreria utilizzando alla linea 14: -lQGLViewer-qt5, ora il progetto compila

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo apt-get install qtcreator qt5-default -y
+sudo apt-get install libboost-all-dev libcgal-dev libgmp-dev libqglviewer-dev -y
+sudo apt-get install git doxygen libeigen3-dev -y
+
